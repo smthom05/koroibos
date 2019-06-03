edce0502577c9b4dd9b2e88e00c4590c3266c7eb
Scott Thomas <smthom05@gmail.com>
2019-06-03 15:46:32 -0600

    Add Event model and migration

---
+++ migrations/20190603214230-create-event.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      SportId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Sport',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Events');
+  }
+};
