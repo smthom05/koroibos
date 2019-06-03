cc0c986f78393f12215a469f6bf569b890f32600
Scott Thomas <smthom05@gmail.com>
2019-06-03 15:53:23 -0600

    Add Sport model and migration

---
+++ models/sport.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Sport = sequelize.define('Sport', {
+    name: DataTypes.STRING
+  }, {});
+  Sport.associate = function(models) {
+    Sport.hasMany(models.Event)
+  };
+  return Sport;
+};
