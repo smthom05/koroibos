3a941ab306e9ab148c39b73b85fce340d83c958c
Scott Thomas <smthom05@gmail.com>
2019-06-04 16:16:05 -0600

    Create seeders for olympians, events, and olympianEvents

---
+++ seeders/20190604220208-olympian_seed.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Olympian', [{
+      id: 1,
+      name: 'Scott Thomas',
+      age: 30,
+      sex: 'M',
+      weight: 67,
+      team: "United States",
+      sport: "Athletics",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 2,
+      name: 'Tim Allen',
+      age: 29,
+      sex: 'M',
+      weight: 47,
+      team: "United States",
+      sport: "Athletics",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 3,
+      name: 'Julia Marcos',
+      age: 24,
+      sex: 'F',
+      weight: 40,
+      team: "Spain",
+      sport: "Athletics",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Olympian', null, {});
+  }
+};
