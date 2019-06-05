610e94a1093c7c0c3515149505fd8c55f414df15
Scott Thomas <smthom05@gmail.com>
2019-06-04 19:54:05 -0600

    Add test for event medalist endpoint and functionality for event medalist endpoint

--- routes/api/v1/events.js
+++ routes/api/v1/events.js
@@ -28,15 +28,39 @@ router.get('/', async function (req, res) {
 router.get('/:id/medalists', async function (req, res) {
   res.setHeader("Content-Type", "application/json");
   try {
-    const events = await Event.findAll({
-      attributes: [
-        'sport',
-        [Sequelize.fn('array_agg', Sequelize.col('name')), 'events']
-      ],
-      group: ['Event.sport']
-    });
-    res.status(200).send({ events });
+    const eventId = req.params.id;
+    const event = await Event.findOne({
+      where: {
+        id: eventId
+      }
+    })
+
+    await OlympianEvent.findAll({
+      where: {
+        EventId: eventId,
+        medal: {[Op.not]: 'NA'}
+      },
+      include: [{
+        model: Olympian,
+        attributes: ['name', 'team', 'age']
+      }]
+    })
+    .then(olympianEvents => {
+      let medalists = olympianEvents.map(function(oe){
+        return {"name": oe.Olympian.name,
+        "team": oe.Olympian.team,
+        "age": oe.Olympian.age,
+        "medal": oe.medal}
+      })
+
+      let medalistFormatted = {
+        "event": event.name,
+        "medalists": medalists
+      }
+      res.status(200).send(medalistFormatted)
+    })
   } catch (error) {
+    console.log(error);
     res.status(404).send({ error });
   }
 })
